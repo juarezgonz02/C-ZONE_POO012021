94d956b1e53f75f87a09a8967d24d2627b3fe62a
Oscar Juárez <juarezgonz02@gmail.com>
2021-03-09 22:10:58 -0600

    Initial Commit

---
+++ hello world/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b; 
+            
+            Console.WriteLine("Enter a value for a: ");
+            a = Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter a value for b: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals {addNumbers(a, b)}");
+        }
+
+        static int addNumbers(int a, int b){
+            return (a + b);
+        }
+    }
+}
